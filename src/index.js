5c7aba6851ce15911019dad0ce4927f31e6d4cbc
Luan Ferreira <luaangmess950@gmail.com>
2025-11-07 15:52:41 -0300

    segundo commit do backend

--- src/index.js
+++ src/index.js
@@ -7,9 +7,23 @@ const port = 3000;
 
 
 app.use(cors())
+app.use(express.json())
 
 app.get("/", (request, response) => {
     response.json(people)
+})
+
+app.post("/cadastrar", (request, response) => {
+    const { name, email, age, password } = request.body.user
+
+    console.log(`
+        Nome: ${name},
+        E-mail: ${email},
+        Age: ${age},
+        Senha: ${password}
+        `)
+
+        response.status(201).json({ message: "Usuário cadastrado com sucesso!" })
 })
 
 app.listen(port,"0.0.0.0", () => {
